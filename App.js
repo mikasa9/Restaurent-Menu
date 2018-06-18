34b96151bc0a7e61eea1c21a3c3571cac520d0dc
Babli Dey <bablihdey@gmail.com>
2018-06-18 15:56:54 +0530

    Stack navigation implemented

--- App.js
+++ App.js
@@ -1,125 +1,19 @@
 import React, { Component } from 'react';
-import {
-    Text,
-    View,
-    Image,
-    TextInput,
-    TouchableOpacity,
-    StyleSheet
-} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import LandingPage from './src/components/LandingPage';
+import Details from './src/components/Details';
 
-export default class App extends Component {
-    render() {
-        return (
-            <View
-                style={styles.container}
-            >
-                <View
-                    style={{
-                        flex: 1.5,
-                        alignItems: 'center',
-                        marginTop: 20,
-                    }}
-                >
-                    <Image
-                        style={styles.imageStyle}
-                        source={{ uri: 'https://qph.ec.quoracdn.net/main-qimg-11604fd8f8b93d630eb6b3a941fd9ec7.webp' }}
-                    />
-                    <Text
-                        style={styles.title}
-                    >
-                        Welcome Back!
-                    </Text>
-                    <Text
-                        style={{ fontSize: 19, color: '#000000', marginTop: 20, }}
-                    >
-                        Introducing yonai,with more than 6 thousand
-                    </Text>
-                    <Text
-                        style={{ fontSize: 19, color: '#000000' }}
-                    >
-                        recepies and amazing features yonai.
-                    </Text>
-                </View>
-                <View
-                    style={{
-                        flex: 1.5,
-                        alignContent: 'flex-start',
-                    }}
-                >
-                    <Text
-                        style={{
-                            fontSize: 20,
-                            justifyContent: 'flex-start',
-                            fontWeight: 'bold',
-                            color: '#000000',
-                        }}
-                    >Email
-                    </Text>
-                    <TextInput
-                        style={styles.inputButton}
-                        placeholder="E-mail"
-                    />
-                    <Text
-                        style={{
-                            fontSize: 20,
-                            marginTop: 20,
-                            justifyContent: 'flex-start',
-                            fontWeight: 'bold',
-                            color: '#000000',
-                        }}
-                    >
-                        Password
-                     </Text>
-                    <TextInput
-                        style={styles.inputButton}
-                        placeholder="Password"
-                    />
-                    <TouchableOpacity
-                        style={styles.button}
-                    >
-                        <Text
-                            style={{ fontSize: 26, color: '#ffffff' }}
-                        >
-                            Login
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        )
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'center'
+export default createStackNavigator({
+    Home: {
+        screen: LandingPage,
+        navigationOptions: {
+            header: null
+        },
     },
-    imageStyle: {
-        marginTop: 20,
-        resizeMode: 'center',
-        height: 100,
-        width: 250
+    Details: {
+        screen: Details,
+        navigationOptions: () => ({
+           title:'Detail',                    
+        })
     },
-    title: {
-        fontSize: 27,
-        marginTop: 25,
-        color: 'rgb(0,0,0)',
-        alignItems: 'center'
-    },
-    inputButton: {
-        margin: 5,
-        width: 350,
-        color: '#ff4500',
-        fontSize: 27,        
-    },
-    button: {
-        marginTop: 25,
-        backgroundColor: '#ff4500',
-        paddingHorizontal: 150,
-        paddingVertical: 15,
-        borderRadius: 7
-    },
-})
+});
